07e50107def72762069f35305984ec8673347ec2
Mathias V. Nielsen <mvn@devpod.dk>
2019-09-12 00:58:43 +0200

    Version 1.0.0
    Root of Project

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-smooth-scroll
+# What is this?
+
+A very light smooth scroller used to scroll from elements to element
+
+# Installation
+
+`npm i light-smooth-scroller`
+
+Then...
+
+```
+import { smoothScroller } from 'light-smooth-scroller';
+
+smoothScroller({
+    selector: '.scroll',
+    header: 'headerClass',
+    definedOffset: 30,
+    smooth: true,
+});
+
+HTML:
+<a id="top" target="bottom" class="scroll">
+    Click me!
+</a>
+<a id="bottom" target="top" class="scroll">
+    Click me!
+</a>
+```
+
+## Options
+
+light-smooth-scroller supports 4 options, 3 which are optional
+
+* *selector* - _class / id_ (Required - Must be a valid class name or ID of element) - No Default Selector
+* *header* - _class_ (Optional - Select the class of your fixed header to calculate needed padding for scroll) - Default: none
+* *definedOffset* - _number_ (Optional - If you want to add more offset to the scroll) - Default: 0
+* *smooth* - _boolean_ (Optional - Defines if you want smooth scrolling or not) - Default: true
---
+++ index.js
@@ -0,0 +1,77 @@
+/**
+ * Main Functions for Smooth Scroller
+ * @param options
+ * Options:
+ *  - Selector: What class or id to target.
+ *  - Smooth: True/False
+ *  - Header: Target class or id to add offset for fixed header
+ *  - Offset: Offset to add in pixels
+ */
+function smoothScroller(options) {
+    if (options.selector.charAt(0) === '#' || options.selector.charAt(0) === '.' && isString(options.selector)) {
+        let targets = document.querySelectorAll(options.selector);
+
+        if (targets.length > 0) {
+            if (options.header === '' || !isString(options.header)) {
+                options.header = false;
+            }
+            if (options.definedOffset === '' || isNaN(options.definedOffset)) {
+                options.definedOffset = 0;
+            }
+            if (options.smooth !== true && options.smooth !== false) {
+                options.smooth = true;
+            }
+            targets.forEach(target => {
+                target.addEventListener("click", function (e) {
+                    scrollToElm(e.currentTarget, options.selector.substr(1), options.header, options.definedOffset, options.smooth);
+                });
+            });
+        } else {
+            throwError(`No objects found for smooth-scroll using selector: ${options.selector}`)
+        }
+    } else {
+        throwError('Invalid selector for smooth-scroll')
+    }
+
+    function scrollToElm(elm, selector, header, definedOffset, behavior) {
+        if (header) {
+            let fixedHeader = document.getElementsByClassName(header);
+            let topOfElement = document.getElementById(elm.target).offsetTop - (fixedHeader[0].clientHeight + definedOffset);
+
+            if (behavior) {
+                window.scroll({
+                    top: topOfElement,
+                    behavior: "smooth",
+                });
+            } else {
+                window.scroll({
+                    top: topOfElement,
+                });
+            }
+
+        } else {
+            let topOfElement = document.getElementById(elm.target).offsetTop - definedOffset;
+
+            if (behavior) {
+                window.scroll({
+                    top: topOfElement,
+                    behavior: "smooth",
+                });
+            } else {
+                window.scroll({
+                    top: topOfElement,
+                });
+            }
+        }
+    }
+
+    function throwError(e) {
+        throw new Error(e);
+    }
+
+    function isString(s) {
+        return typeof s === 'string' || s instanceof String;
+    }
+}
+
+module.exports.smoothScroller = smoothScroller;
